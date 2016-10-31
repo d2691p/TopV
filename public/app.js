eb05c26137c4fe73ca2a1797628d13387aa0d09e
Dom Painter <dominick.painter@grg.com>
2016-10-31 16:54:08 +0000

    Layout, tabulation, pagination added

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ const API_KEY = 'AIzaSyDwtfXnk_ZPb73htuCztM0Jq9HKWJYT0Zc';
 const SEARCH_CHANNEL_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel';		//requires &key, &q
 const CHANNELS_DETAILS_URL = 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails';			//requires &id
 const PLAYLIST_ITEMS_URL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet';				//requires &playlistId
-const VIDEO_DETAILS_URL = 'https://www.googleapis.com/youtube/v3/videos?part=statistics'; 		//requires &id 
+const VIDEO_DETAILS_URL = 'https://www.googleapis.com/youtube/v3/videos?part=statistics'; 					//requires &id 
 
 app.controller("topVController", ['$http','$scope', function($http, $scope){
 	
@@ -12,6 +12,13 @@ app.controller("topVController", ['$http','$scope', function($http, $scope){
 	$scope.channelSnippets = [];
 	$scope.uploadsid = "";
 	$scope.videos = [];
+	$scope.sortType = 'upload';
+	$scope.sortReverse = false;
+	$scope.groupedItems = [];
+    $scope.itemsPerPage = 10;
+    $scope.pagedItems = [];
+    $scope.currentPage = 0;
+    $scope.gap = 5;
 
 	$scope.search = function (term) {
 		$http.get(SEARCH_CHANNEL_URL + '&key=' + API_KEY + '&q=' + term)
@@ -34,10 +41,11 @@ app.controller("topVController", ['$http','$scope', function($http, $scope){
 				console.log("playlistId: " + playlistId);
 
 				angular.forEach($scope.videos, function(video){
-					console.log("go for each");
 					$scope.getVideoDetails(video);
-					console.log($scope.videos);
 				});
+
+				$scope.groupToPages();		//For pagination
+			
 			})
 			.error(function(data){
 				console.log("ERROR");
@@ -71,6 +79,54 @@ app.controller("topVController", ['$http','$scope', function($http, $scope){
 				console.log("ERROR");
 			});
 	};
+
+
+    $scope.groupToPages = function () {
+        $scope.pagedItems = [];
+        
+        for (var i = 0; i < $scope.videos.length; i++) {
+            if (i % $scope.itemsPerPage === 0) {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.videos[i] ];
+            } else {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.videos[i]);
+            }
+        }
+
+        console.log($scope.pagedItems);
+    };
+
+    $scope.range = function (size,start, end) {
+        var ret = [];        
+                      
+        if (size < end) {
+            end = size;
+            start = size-$scope.gap;
+        }
+        for (var i = start; i < end; i++) {
+            ret.push(i);
+        }        
+        return ret;
+    };
+
+	$scope.setSort = function(sortBy){
+		$scope.sortType = sortBy;
+	};
+
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 0) {
+            $scope.currentPage--;
+        }
+    };
+    
+    $scope.nextPage = function () {
+        if ($scope.currentPage < $scope.pagedItems.length - 1) {
+            $scope.currentPage++;
+        }
+    };
+    
+    $scope.setPage = function () {
+        $scope.currentPage = this.n;
+    };
 
 }]);
 
